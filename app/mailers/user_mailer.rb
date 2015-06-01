3b61ffc8537d9ae94102429014e5ce93e34b8db8
Jen Luo <jen.luo@live.ca>
2015-06-01 00:18:11 -0400

    heroku try 4

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,3 @@
 class UserMailer < ApplicationMailer
-	default from: "spacebuq@gmail.com"
 
-	def welcome_email(user)
-		@user = user
-		@url = 'http://spacebuq.com'
-		mail(to: @user.email, subject: "Test Welcome Email")
-	end
 end
