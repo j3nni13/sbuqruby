e0808a30da08dca722ce9944fbaf49316ee3430f
Jen Luo <jen.luo@live.ca>
2015-06-01 00:32:54 -0400

    heroku7

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,8 +81,8 @@ ActionMailer::Base.smtp_settings = {
   :address        => 'smtp.sendgrid.net',
   :port           => '587',
   :authentication => :plain,
-  :user_name      => ENV['app36605013@heroku.com'],
-  :password       => ENV['stfit6nw1855'],
+  :user_name      => ENV['SENDGRID_USERNAME']
+  :password       => ENV['SENDGRID_PASSWORD'],
   :domain         => 'heroku.com',
   :enable_starttls_auto => true
 }
